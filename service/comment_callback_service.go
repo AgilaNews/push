d44bb0057bcf275835c5d030a0c7366a2bf29f0a
zhangguanxing <zgxcassar@gmaol.com>
2016-11-17 13:44:56 +0800

    add all

--- service/comment_callback_service.go
+++ service/comment_callback_service.go
@@ -17,21 +17,17 @@ func NewCommentCallbackService() (*CommentCallbackService, error) {
 	return &CommentCallbackService{}, nil
 }
 
-func (s *CommentCallbackService) OnReply(ctx context.Context, req *pb.OnReplyCallbackRequest) (*pb.GeneralResponse, error) {
-	resp := &pb.GeneralResponse{}
+func (s *CommentCallbackService) OnReply(ctx context.Context, req *pb.OnReplyCallbackRequest) (*pb.EmptyMessage, error) {
+	resp := &pb.EmptyMessage{}
 
 	if req.Comment.RefComment == nil {
-		resp.Code = pb.GeneralResponse_REQ_PARAM_ERROR
-		resp.ErrorMsg = fmt.Sprintf("unfind ref comment")
-		log4go.Warn("unfind ref comment")
+		return nil, fmt.Errorf("unfind ref comment")
 		return resp, nil
 	}
 	log4go.Info("on received comment reply event: user:%v", req.Comment.RefComment)
 
 	if devices, err := device.GlobalDeviceMapper.GetDeviceByUserId(req.Comment.RefComment.UserId); err != nil {
-		resp.Code = pb.GeneralResponse_INTERNAL_ERROR
-		resp.ErrorMsg = fmt.Sprintf("get device of %v error", req.Comment.RefComment.UserId)
-		return resp, nil
+		return nil, fmt.Errorf("get device of %v error", req.Comment.RefComment.UserId)
 	} else {
 		log4go.Info("pushed notifications to %d device", len(devices))
 		for _, d := range devices {
@@ -40,7 +36,11 @@ func (s *CommentCallbackService) OnReply(ctx context.Context, req *pb.OnReplyCal
 		}
 	}
 
-	resp.Code = pb.GeneralResponse_NO_ERROR
-	resp.ErrorMsg = "ok"
+	return resp, nil
+}
+
+func (s *CommentCallbackService) OnLiked(ctx context.Context, req *pb.OnLikedCallbackRequest) (*pb.EmptyMessage, error) {
+	resp := &pb.EmptyMessage{}
+
 	return resp, nil
 }
